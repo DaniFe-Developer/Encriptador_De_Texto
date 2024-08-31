228c3303c07fc89e2688b4757df327b7b04956f4
DaniFe-Developer <d.pinto142004@gmail.com>
2024-08-31 17:23:43 -0500

    Proyecto challenger encriptador

---
+++ JS/script.js
@@ -0,0 +1,133 @@
+//Selccion elementos
+
+const btnEncriptar = document.querySelector(".btn-encriptar");
+const txtEncriptar = document.querySelector(".encriptar");
+const aviso = document.querySelector(".texto-aviso");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector(".tarjeta-contendor");
+const btnCopiar = document.querySelector(".btn-copiar");
+const btnDesencriptar = document.querySelector(".btn-desencriptar");
+
+
+//Boton encriptar
+
+btnEncriptar.addEventListener("click", e => {
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ");
+    
+    
+    if(texto== ""){
+        aviso.style.backgroundColor = "#0A3871";
+        aviso.style.color = "#fff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El campo de texto no debe esta vacio";
+
+        setTimeout(() => {
+          aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== txt){
+        aviso.style.backgroundColor = "#0A3871";
+        aviso.style.color = "#fff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "No debe tener asentos ni caracteres especiales";
+
+        setTimeout(() => {
+          aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        aviso.style.backgroundColor = "#0A3871";
+        aviso.style.color = "#fff";
+        aviso.style.fontWeight = "800";
+        aviso.textContent = "El texto debe ser todo en minuscula";
+
+        setTimeout(() => {
+          aviso.removeAttribute("style");
+        },1500); 
+    }
+
+    else{
+        texto = texto.replace(/e/mg, "enter");
+        texto = texto.replace(/i/mg, "imes");
+        texto = texto.replace(/a/mg, "ai");
+        texto = texto.replace(/o/mg, "ober");
+        texto = texto.replace(/u/mg, "ufat");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove();
+    }
+
+});
+
+
+
+//Boton desencriptar
+
+btnDesencriptar.addEventListener("click", e => {
+  e.preventDefault();
+  let texto = txtEncriptar.value;
+  let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ");
+  
+  
+  if(texto== ""){
+      aviso.style.backgroundColor = "#0A3871";
+      aviso.style.color = "#fff";
+      aviso.style.fontWeight = "800";
+      aviso.textContent = "El campo de texto no debe esta vacio";
+
+      setTimeout(() => {
+        aviso.removeAttribute("style");
+      },1500);
+  }
+
+  else if(texto !== txt){
+      aviso.style.backgroundColor = "#0A3871";
+      aviso.style.color = "#fff";
+      aviso.style.fontWeight = "800";
+      aviso.textContent = "No debe tener asentos ni caracteres especiales";
+
+      setTimeout(() => {
+        aviso.removeAttribute("style");
+      },1500);
+  }
+
+  else if(texto !== texto.toLowerCase()){
+      aviso.style.backgroundColor = "#0A3871";
+      aviso.style.color = "#fff";
+      aviso.style.fontWeight = "800";
+      aviso.textContent = "El texto debe ser todo en minuscula";
+
+      setTimeout(() => {
+        aviso.removeAttribute("style");
+      },1500); 
+  }
+
+  else{
+      texto = texto.replace(/enter/mg, "e");
+      texto = texto.replace(/imes/mg, "i");
+      texto = texto.replace(/ai/mg, "a");
+      texto = texto.replace(/ober/mg, "o");
+      texto = texto.replace(/ufat/mg, "u");
+
+      respuesta.innerHTML = texto;
+      btnCopiar.style.visibility = "inherit";
+      contenido.remove();
+  }
+
+});
+
+
+
+//Boton copiar
+
+btnCopiar.addEventListener("click", e => {
+  e.preventDefault();
+  let copiar = respuesta;
+  copiar.select();
+  document.execCommand("copy");
+});
